deca736cb5038b736c5dd8ccd6e84d8f844d2239
alalhossain94 <alalhossain557@gmail.com>
2023-12-30 19:10:38 +0600

    changes

--- Library_Management/settings.py
+++ Library_Management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-jcpyn@rarj$wr*o@0(p*sf=agzrbxy(v)i19p@518$c5j(+_dz'
+SECRET_KEY = 'django-insecure-jcpyn@rarj$wr*o@0(p*sf=agzrbxy(v)i19p@518$c5j(+_dz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
