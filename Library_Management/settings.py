e05c634182a92200993c114ab5606a4b491984ee
alalhossain94 <alalhossain557@gmail.com>
2023-12-31 13:38:31 +0600

    new

--- Library_Management/settings.py
+++ Library_Management/settings.py
@@ -24,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jcpyn@rarj$wr*o@0(p*sf=agzrbxy(v)i19p@518$c5j(+_dz'
+# SECRET_KEY = 'django-insecure-jcpyn@rarj$wr*o@0(p*sf=agzrbxy(v)i19p@518$c5j(+_dz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://library-management-7auh.onrender.com','https://*.127.0.0.1']
+# CSRF_TRUSTED_ORIGINS = ['https://library-management-7auh.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
@@ -86,13 +86,24 @@ WSGI_APPLICATION = 'Library_Management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+SECRET_KEY = env("SECRET_KEY")
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
-
 
 
 
